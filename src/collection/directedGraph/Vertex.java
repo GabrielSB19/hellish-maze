d21ebd39e8c4f307b12fad74c61c657d053ac969
alejandro945 <alejo8677@gmail.com>
2021-11-13 18:47:53 -0500

    :zap: Props

--- src/collection/directedGraph/Vertex.java
+++ src/collection/directedGraph/Vertex.java
@@ -2,7 +2,24 @@ package collection.directedGraph;
 
 import java.util.LinkedList;
 
-public class Vertex<V,E> {
+public class Vertex<V, E> {
     private V data;
-    private LinkedList<Edge<V,E>> edges;
+    private LinkedList<Edge<V, E>> edges;
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public V getData() {
+        return this.data;
+    }
+
+    public LinkedList<Edge<V, E>> getEdges() {
+        return this.edges;
+    }
+
+    public void setEdges(LinkedList<Edge<V, E>> edges) {
+        this.edges = edges;
+    }
+
 }
