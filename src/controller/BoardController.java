09c041c9671e492db261e741aa5f378ff6b76631
alejandro945 <alejo8677@gmail.com>
2021-11-15 15:35:37 -0500

    :seedling: Finish Board

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -27,8 +27,6 @@ public class BoardController {
     @FXML
     private ImageView hasKey;
 
-    @FXML
-    private ImageView notKey;
     private MazeController mController;
     private int avatar;
 
@@ -45,21 +43,21 @@ public class BoardController {
         mController.getMaze().createGraph(EASY_V);
         createRooms(EASY_V, 1);
         createDoors((int) Math.sqrt(EASY_V), 1);
-        createBoard(EASY_V);
+        createBoard(EASY_V, "green");
     }
 
     public void mediumBoard() {
         mController.getMaze().createGraph(MEDIUM_V);
         createRooms(MEDIUM_V, 2);
         createDoors((int) Math.sqrt(MEDIUM_V), 2);
-        createBoard(MEDIUM_V);
+        createBoard(MEDIUM_V, "yellow");
     }
 
     public void hardBoard() {
         mController.getMaze().createGraph(HARD_V);
         createRooms(HARD_V, 3);
         createDoors((int) Math.sqrt(HARD_V), 3);
-        createBoard(HARD_V);
+        createBoard(HARD_V, "red");
     }
 
     private void createRooms(int rooms, int type) {
@@ -82,7 +80,7 @@ public class BoardController {
         }
     }
 
-    private void createBoard(int amount) {
+    private void createBoard(int amount, String color) {
         Maze m = mController.getMaze();
         board.getChildren().clear();
         int rowsAndCols = (int) Math.sqrt(amount);
@@ -101,10 +99,10 @@ public class BoardController {
                     Door current = m.getGraph().getVertex(idRoom).getEdges().get(k).getData();
                     Room s = m.getGraph().getVertex(idRoom).getEdges().get(k).getSource().getData();
                     Room d = m.getGraph().getVertex(idRoom).getEdges().get(k).getDestination().getData();
-                    
+
                     int source = (s.getId() == idRoom) ? s.getId() : d.getId();
                     int destination = (d.getId() == idRoom) ? s.getId() : d.getId();
-                    int[] pos = getPosition(source, destination);
+                    int[] pos = getPosition(source, destination, rowsAndCols);
 
                     ImageView dImg = doorImage(current);
                     Label dToken = new Label(current.getToken() + "");
@@ -112,19 +110,20 @@ public class BoardController {
                     tempGrid.add(dImg, pos[0], pos[1]);
                     tempGrid.add(dToken, pos[2], pos[3]);
                 }
-                tempGrid.setStyle("-fx-border-color: red; -fx-border-radius: 40px; -fx-background-radius: 40px;");
+                tempGrid.setStyle(
+                        "-fx-border-color:" + color + "; -fx-border-radius: 20px; -fx-background-radius: 20px;");
                 boxStyle(tempGrid, 0, tempGrid.getChildren().size());
                 board.add(tempGrid, j, i);
                 idRoom++;
             }
         }
         boxStyle(board, 0, board.getChildren().size());
-        board.setHgap(15);
-        board.setVgap(15);
-        board.setPadding(new Insets(15, 15, 15, 15));
+        board.setHgap(10);
+        board.setVgap(10);
+        board.setPadding(new Insets(10, 10, 10, 10));
     }
 
-    private int[] getPosition(int s, int d) {
+    private int[] getPosition(int s, int d, int props) {
         int[] pos = new int[4];
         if (s - d == 1) {
             pos[0] = 0;
@@ -136,7 +135,7 @@ public class BoardController {
             pos[1] = 1;
             pos[2] = 3;
             pos[3] = 2;
-        } else if (s - d == 3) {
+        } else if (s - d == props) {
             pos[0] = 1;
             pos[1] = 0;
             pos[2] = 2;
@@ -151,8 +150,8 @@ public class BoardController {
     }
 
     private void renderImg(ImageView img) {
-        img.setFitHeight(35);
-        img.setFitWidth(35);
+        img.setFitHeight(20);
+        img.setFitWidth(20);
     }
 
     private ImageView roomImage(Room r) {
@@ -203,16 +202,16 @@ public class BoardController {
     public void movement(KeyEvent event) {
         switch (event.getCode()) {
         case UP:
-
+            System.out.println("up");
             break;
         case DOWN:
-
+            System.out.println("down");
             break;
         case RIGHT:
-
+            System.out.println("r");
             break;
         case LEFT:
-
+            System.out.println("l");
             break;
         default:
             break;
