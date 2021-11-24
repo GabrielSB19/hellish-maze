3b35ee8509de66ecf1ec817abe23b680dd3d93f7
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2021-11-23 23:17:12 -0500

    Finish the test in Maze, finish all test

--- test/model/MazeTest.java
+++ test/model/MazeTest.java
@@ -1,44 +1,57 @@
 package model;
 
+import collection.Graph;
+import collection.UndirectedGraph;
+import model.door.Door;
+import model.door.GoldenDoor;
+import model.door.InfernalDoor;
+import model.door.SilverDoor;
+import model.room.EnchantedRoom;
+import model.room.KeyRoom;
+import model.room.Room;
+import model.room.TraditionalRoom;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MazeTest {
 
+    public void setUpScenary1(){}
+
     @Test
     void createGraph() {
+        setUpScenary1();
+        Graph<Room, Door> graph = new UndirectedGraph<>(15);
+        assertNotNull(graph);
     }
 
     @Test
     void addRoom() {
+        Room room1 = new TraditionalRoom(0);
+        Room room2 = new KeyRoom(1);
+        Room room3 = new EnchantedRoom(2);
+
+        assertNotNull(room1);
+        assertNotNull(room2);
+        assertNotNull(room3);
     }
 
     @Test
     void addDoor() {
+        Door door1 = new SilverDoor();
+        Door door2 = new GoldenDoor();
+        Door door3 = new InfernalDoor();
+
+        assertNotNull(door1);
+        assertNotNull(door2);
+        assertNotNull(door3);
     }
 
     @Test
     void setTreasure() {
-    }
+        TraditionalRoom room1 = new TraditionalRoom(2);
+        room1.setTreasure(true);
 
-    @Test
-    void newPlayer() {
-    }
-
-    @Test
-    void getGraph() {
-    }
-
-    @Test
-    void minimumPathBetweenPairs() {
-    }
-
-    @Test
-    void minimumPath() {
-    }
-
-    @Test
-    void getPath() {
+        assertTrue(room1.hasTreasure());
     }
 }
