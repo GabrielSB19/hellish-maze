ab5dc8a92188566f7e588b89f1fe562bb10867cc
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2021-11-23 22:47:28 -0500

    Finish test in undirected graphs

--- test/collection/GraphTest.java
+++ test/collection/GraphTest.java
@@ -123,4 +123,91 @@ class GraphTest {
 
         assertEquals(graph.getVertex(2).getData().getId(), graph.floydWarshall(1));
     }
+
+    @Test
+    void addEdgeUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        TraditionalRoom R1 = new TraditionalRoom(0);
+        TraditionalRoom R2 = new TraditionalRoom(1);
+        SilverDoor D1 = new SilverDoor();
+        graph.addVertex(R1);
+        graph.addVertex(R2);
+        graph.addEdge(D1, R1, R2);
+
+        assertNotNull(graph.getVertex(0).getEdges().get(0));
+    }
+
+    @Test
+    void addVertexUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        TraditionalRoom R1 = new TraditionalRoom(0);
+        graph.addVertex(R1);
+
+        assertNotNull(graph.getVertex(0));
+    }
+
+    @Test
+    void getVertexUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        assertNotNull(graph.getVertex(0));
+    }
+
+    @Test
+    void searchVertexUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        Vertex<Room, Door> test = graph.searchVertex(graph.getVertex(2).getData());
+        assertEquals(2, test.getData().getId());
+    }
+
+    @Test
+    void fillMatrixUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        /*
+        The fill matrix method was realized in the initGraph method.
+         */
+        int weight = graph.getVertex(0).getEdges().get(0).getData().getToken();
+
+        assertNotNull(graph.adjacencyMatrix);
+        assertEquals(weight, graph.adjacencyMatrix[0][1]);
+    }
+
+    @Test
+    void initMatrixUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        graph.initMatrix();
+        assertEquals(0, graph.adjacencyMatrix[0][0]);
+        assertEquals(0, graph.adjacencyMatrix[1][1]);
+    }
+
+    @Test
+    void dijkstraUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        int dist = graph.dijkstra(0);
+        int token1 = (graph.getVertex(0).getEdges().get(0).getData().getToken());
+        int token2 = (graph.getVertex(1).getEdges().get(0).getData().getToken());
+        int tokens = token1+token2;
+        assertEquals(tokens, dist);
+    }
+
+    @Test
+    void getPathUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+        graph.dijkstra(0);
+        String path= "["+graph.getVertex(1).getData().getId()+", "+graph.getVertex(2).getData().getId()+"]";
+        assertEquals(path, graph.getPath());
+    }
+
+    @Test
+    void floydWarshallUD() {
+        UndirectedGraph<Room, Door> graph = setUpScenary2();
+        initGraph(graph);
+
+        assertEquals(graph.getVertex(2).getData().getId(), graph.floydWarshall(1));
+    }
 }
